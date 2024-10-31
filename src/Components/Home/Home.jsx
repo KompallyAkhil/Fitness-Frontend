47483f1bf140518c9760867c385fac0ce3a31a9a
Akki-luffy <akhilkompally27@gmail.com>
2024-10-31 17:46:47 +0530

    Adding chatbot section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,12 @@
 import videos from "./video2.mp4";
 import "./Home.css";
 import genie from "./bot.png"
+import { useState } from "react";
 const Home = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const openChat = () => {
+        setIsOpen(!isOpen)
+    }
     return (
         <>
             <div className="video-container">
@@ -12,14 +17,18 @@ const Home = () => {
                 <div className="onVideo">
                     <div className="inside-video">
                         <h1>BEGIN YOUR FITNESS JOURNEY</h1>
-                        <button className="video-btn">Get Started</button>
+                        <button className="video-btn" onClick={openChat}>Get Started</button>
                     </div>
                 </div>
             </div>
             <div className="model-container">
-                <button><img className="model-image" src={genie}/></button>
-        </div>
-            
+                <button><img className="model-image" onClick={openChat} src={genie} /></button>
+            </div>
+            {isOpen && (
+                <div className="bot-container">
+                    <h1>Hello</h1>
+                </div>)}
+
         </>
     )
 }
